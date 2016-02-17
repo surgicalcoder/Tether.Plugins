28c9f6c4fb19c2beb99a9c3d59807f487822dfb4
Monty A <ma@surgicalcoder.com>
2016-02-17 12:13:00 +0000

    Added new ASPNET requests plugin, and version change for plugin API.

---
+++ Tether.ASPNetRequests/ASPNETApplication.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tether.Plugins;
+
+namespace Tether.ASPNetRequests
+{
+    [PerformanceCounterGrouping("ASP.NET Applications", SelectorEnum.Each, UsePerformanceCounter = true)]
+    public class ASPNETApplication
+    {
+        [PerformanceCounterValue("% Managed Processor Time (estimated)")]
+        public long PercentProcessorTime { get; set; }
+
+        [PerformanceCounterValue("Errors Total/Sec")]
+        public long ErrorsPerSec { get; set; }
+
+        [PerformanceCounterValue("Pipeline Instance Count")]
+        public long PipelineInstanceCount { get; set; }
+
+        [PerformanceCounterValue("Request Execution Time")]
+        public long RequestExecutionTime { get; set; }
+
+        [PerformanceCounterValue("Request Wait Time")]
+        public long RequestWaitTime { get; set; }
+
+        [PerformanceCounterValue("Requests In Application Queue")]
+        public long RequestsInApplicaitonQueue { get; set; }
+
+        [PerformanceCounterValue("Requests/Sec")]
+        public long RequestsPerSec { get; set; }
+
+        private string name;
+
+        [PerformanceCounterInstanceName]
+        public string Name
+        {
+            get { return name; }
+            set { name = value.Replace("_LM_W3SVC_", ""); }
+        }
+    }
+}

db2df215e53efbb9e30ee19b316c315a1d073768
Monty A <monty@dotnetpro.co.uk>
2017-08-29 09:59:45 +0100

    Disposal

--- Tether.HTTPServiceRequestQueues/ASPNETGlobalQueues.cs
+++ Tether.HTTPServiceRequestQueues/ASPNETGlobalQueues.cs
@@ -13,13 +13,17 @@ namespace Tether.HTTPServiceRequestQueues
             IDictionary<string, object> values = new Dictionary<string, object>();
 
             var v2Category = new PerformanceCounterCategory("ASP.NET v2.0.50727");
-            values.Add("v2-Requests-Queued", v2Category.GetCounters().FirstOrDefault(e => e.CounterName == "Requests Queued").NextValue());
-            var v4Category = new PerformanceCounterCategory("ASP.NET v4.0.30319");
-            values.Add("v4-Requests-Queued", v4Category.GetCounters().FirstOrDefault(e => e.CounterName == "Requests Queued").NextValue());
-            values.Add("v4-Requests-NativeQueued", v4Category.GetCounters().FirstOrDefault(e => e.CounterName == "Requests In Native Queue").NextValue());
+            var v2Counters = v2Category.GetCounters();
 
-            DisposeAll(v2Category.GetCounters());
-            DisposeAll(v4Category.GetCounters());
+            var v4Category = new PerformanceCounterCategory("ASP.NET v4.0.30319");
+            var v4Counters = v4Category.GetCounters();
+
+            values.Add("v2-Requests-Queued", v2Counters.FirstOrDefault(e => e.CounterName == "Requests Queued").NextValue());
+            values.Add("v4-Requests-Queued", v4Counters.FirstOrDefault(e => e.CounterName == "Requests Queued").NextValue());
+            values.Add("v4-Requests-NativeQueued", v4Counters.FirstOrDefault(e => e.CounterName == "Requests In Native Queue").NextValue());
+
+            DisposeAll(v2Counters);
+            DisposeAll(v4Counters);
 
             return values;
         }
